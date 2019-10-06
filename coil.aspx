27bde9e70e53093936ff78dc52acce0afa436794
gamexy <gamexy@msn.com>
2019-10-06 14:16:16 +0800

    Add files via upload

---
+++ coil.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="coil.aspx.cs" Inherits="test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+
+        .auto-style2 {
+            width: 65%;
+        }
+
+        .Freezing {
+            position: relative;
+            table-layout: fixed;
+            top: expression(this.offsetParent.scrollTop);
+            z-index: 10;
+        }
+
+            .Freezing th {
+                text-overflow: ellipsis;
+                overflow: hidden;
+                white-space: nowrap;
+                padding: 2px;
+            }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+            <asp:Timer ID="Timer1" runat="server" OnTick="Timer1_Tick" Interval="600000">
+            </asp:Timer>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click"
+                Text="刷新" />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" BackColor="White"
+                BorderColor="#CCCCCC" BorderStyle="Solid" BorderWidth="1px" CellPadding="3"
+                Font-Size="X-Large">
+                <Columns>
+                    <asp:ButtonField ButtonType="Button" Text="选择" CommandName="select" />
+                </Columns>
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+            </asp:GridView>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">
+
+                        <asp:Panel ID="Panel1" runat="server" ScrollBars="Auto" Height="680px" Width="800px">
+                            <asp:GridView ID="GridView_detail" runat="server" CellPadding="4"
+                                ForeColor="#333333" BorderStyle="Solid" BorderWidth="1px" OnRowDataBound="GridView_detail_RowDataBound">
+                                <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                                <EditRowStyle BackColor="#999999" />
+                                <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                                <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                                <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                                <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                                <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                                <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                                <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                                <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                                <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+                            </asp:GridView>
+                        </asp:Panel>
+                    </td>
+                    <td style="vertical-align: top">
+                        <asp:GridView ID="GridView_gy" runat="server" BorderStyle="Solid" BorderWidth="2px" CellPadding="10" OnRowDataBound="GridView_gy_RowDataBound" ForeColor="#333333">
+                            <AlternatingRowStyle BackColor="#5D7B9D" ForeColor="#FFFFFF" />
+                            <EditRowStyle BackColor="#999999" />
+                            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+                        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ coil.aspx.cs
@@ -0,0 +1,221 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        refresh();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        GetCoil();
+    }
+
+    private void GetCoil()
+    {
+        using (OracleConnection myConn = new OracleConnection())
+        {
+            myConn.ConnectionString = @"Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =10.1.1.22)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = l3db))); User Id = l3dbuser;Password = l3dbuser;";
+
+            myConn.Open();
+
+            OracleCommand oracleCommand = new OracleCommand
+            {
+                Connection = myConn,
+                CommandType = CommandType.StoredProcedure,
+                CommandText = "GY_REQUIRE"
+            };
+            oracleCommand.ExecuteNonQuery();
+
+            myConn.Close();
+        }
+    }
+
+    private void refresh()
+    {
+        DataTable mytable = new DataTable();
+        using (OracleConnection myConn = new OracleConnection())
+        {
+            myConn.ConnectionString = @"Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =10.1.1.22)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = l3db))); User Id = l3dbuser;Password = l3dbuser;";
+
+            myConn.Open();
+
+            //OracleCommand oracleCommand = new OracleCommand
+            //{
+            //    Connection = myConn,
+            //    CommandType = CommandType.StoredProcedure,
+            //    CommandText = "GY_REQUIRE"
+            //};
+            //oracleCommand.ExecuteNonQuery();
+
+            OracleCommand oraCmd = new OracleCommand();
+            oraCmd.Connection = myConn;
+
+            oraCmd.CommandText = string.Format(@"select 单位名,slab_numbers 块数,sx,状态,品种报信 from （select unitname as 单位名,count(*) as 块数,max(identify_order) as sx
+                ,(case  when max(STATUSFLAG)=100 then '未入炉' when max(STATUSFLAG)>100 then '已入炉' end ) as 状态,wm_concat(rules_name) 品种报信 from (select furnace_query.*,rules_name from furnace_query left join rules_coil on furnace_query.coilno=rules_coil.coilno where del_flag<>1) t group by unitname order by sx) t1 
+                inner join L3_PDI_MAIN on t1.单位名=L3_PDI_MAIN.unit_name ");
+
+            OracleDataAdapter Da = new OracleDataAdapter(oraCmd);
+            Da.Fill(mytable);
+            myConn.Close();
+        }
+        mytable.Columns.Remove("sx");
+
+        for (int i = 0; i < mytable.Rows.Count; i++)//去掉重复内容
+        {
+            if (mytable.Rows[i]["品种报信"] != null)
+            {
+                string[] rs = mytable.Rows[i]["品种报信"].ToString().Split(',');
+                string t = rs[0];
+                foreach (string s in rs)
+                {
+                    if (t.IndexOf(s) < 0)//如果结果字符串不包括它，就加进去
+                    {
+                        t += "," + s;
+                    }
+                }
+                mytable.Rows[i]["品种报信"] = t;
+            }
+        }
+
+        GridView1.DataSource = mytable;
+        GridView1.DataBind();
+        GridView1.RowCommand += new GridViewCommandEventHandler(GridView1_RowCommand);
+    }
+
+    private void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "select")
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            string unitname = GridView1.Rows[index].Cells[1].Text;
+
+            DataTable mytable = new DataTable();
+            using (OracleConnection myConn = new OracleConnection())
+            {
+                myConn.ConnectionString = @"Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =10.1.1.22)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = l3db))); User Id = l3dbuser;Password = l3dbuser;";
+
+                myConn.Open();
+
+                OracleCommand oraCmd = new OracleCommand();
+                oraCmd.Connection = myConn;
+
+                oraCmd.CommandText = string.Format(@"select
+                    furnace_query.COILNO 钢卷号,MSRD 钢种,PSRD 成品材质,decode(ssflag,'S','是') 剪,rules 工艺要点,COILTHICK 厚,COILWIDTH 宽,RT4AIM RT4,FT7AIM FT7,CTAIM CT,SLABNO 板坯号,SLABTHICK 坯厚,SLABWIDTH 坯宽,SLABLENGTH 坯长,NEXTPROCESS 下工序
+                    from furnace_query left join
+                    (select coilno,wm_concat(rules_name) rules from rules_coil group by coilno ) r
+                    on furnace_query.coilno=r.coilno where unitname='{0}' and del_flag<>1 order by identify_order", unitname);
+
+                OracleDataAdapter Da = new OracleDataAdapter(oraCmd);
+                Da.Fill(mytable);
+                myConn.Close();
+            }
+
+            GridView_detail.DataSource = mytable;
+            GridView_detail.DataBind();
+
+            //Response.Write(unitname);
+
+            string gy_name = HttpUtility.HtmlDecode(GridView1.Rows[index].Cells[4].Text).Trim();
+            DataTable gytable = new DataTable();
+            using (OracleConnection myConn = new OracleConnection())
+            {
+                myConn.ConnectionString = @"Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =10.1.1.22)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = l3db))); User Id = l3dbuser;Password = l3dbuser;";
+
+                myConn.Open();
+
+                OracleCommand oraCmd = new OracleCommand();
+                oraCmd.Connection = myConn;
+
+                if (gy_name.Length > 0)
+                {
+                    string[] rs = gy_name.Split(',');
+                    string t = string.Format("rule_name like '{0}'", rs[0]);
+                    foreach (string s in rs)
+                    {
+                        if (t.IndexOf(s) < 0)
+                        {
+                            t += string.Format(" or rule_name like '{0}'", s);
+                        }
+                    }
+                    oraCmd.CommandText = string.Format(@"select rule_name 工艺要点,rule_detail 具体要求 from rules where {0}", t);
+
+                    OracleDataAdapter Da = new OracleDataAdapter(oraCmd);
+                    Da.Fill(gytable);
+                }
+                else
+                {
+                    gytable.Clear();
+                }
+                myConn.Close();
+            }
+
+            //换行 需和绑定事件配合使用
+            for (int i = 0; i < gytable.Rows.Count; i++)
+            {
+                gytable.Rows[i]["具体要求"] = gytable.Rows[i]["具体要求"].ToString().Replace("\n", "<br />");
+                //gytable.Rows[i]["具体要求"] = Server.HtmlEncode(gytable.Rows[i]["具体要求"].ToString());
+            }
+            //if (gytable.Rows.Count > 0)
+            {
+                GridView_gy.DataSource = gytable;
+                GridView_gy.DataBind();
+                for (int i = 0; i < gytable.Rows.Count; i++)
+                {
+                    GridView_gy.Rows[i].Cells[0].Font.Bold = true;
+                }
+            }
+        }
+        //throw new NotImplementedException();
+    }
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        refresh();
+    }
+
+    protected void GridView_detail_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow || e.Row.RowType == DataControlRowType.Header)
+        {
+            //保持列不变形
+            for (int i = 0; i < e.Row.Cells.Count; i++)
+            {
+                //方法一：
+                //e.Row.Cells[i].Text = "&nbsp;" + e.Row.Cells[i].Text + "&nbsp;";
+                e.Row.Cells[i].Text = e.Row.Cells[i].Text;
+                e.Row.Cells[i].Wrap = false;
+
+                //my
+                e.Row.Cells[i].ToolTip = e.Row.Cells[i].Text;
+                if (e.Row.Cells[i].Text.Length > 10)
+                {
+                    e.Row.Cells[i].Text = e.Row.Cells[i].Text.Substring(0, 10);
+                }
+
+                //方法二：
+                //e.Row.Cells[i].Text = "<nobr>&nbsp;" + e.Row.Cells[i].Text + "&nbsp;</nobr>";
+            }
+        }
+    }
+
+    protected void GridView_gy_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            TableCellCollection cells = e.Row.Cells;
+            foreach (TableCell cell in cells)
+            {
+                cell.Text = Server.HtmlDecode(cell.Text);
+            }
+        }
+    }
+}
